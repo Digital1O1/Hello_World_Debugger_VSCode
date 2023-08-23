398df6839aa9b32e297c6da82cef27919911a048
Digital1O1 <ChristopherTa89@gmail.com>
2023-08-23 17:09:02 +0100

    first push

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    printf("Program started...\r\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Program iteration [ %d ] \r\n", i);
+    }
+
+    printf("Program ended...\r\n");
+
+    return 0;
+}
